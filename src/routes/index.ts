0056ffb0b1a270b4c42e8b815cd8b20dd7fdca10
Luan <luan.santos@rspneus.com.br>
2025-03-13 11:16:43 -0300

    Adicionado rota de deletar contato

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,4 +1,4 @@
-import express from "express";
+import express, { Router } from "express";
 import { readFile, writeFile } from "fs/promises";
 
 const dataSource = "./data/list.txt";
@@ -24,6 +24,39 @@ router.post("/contato", async (req, res) => {
   await writeFile(dataSource, list.join("\n"));
 
   res.status(201).json({ contato: name });
+});
+
+router.get("/contatos", async (req, res) => {
+  let list: string[] = [];
+  try {
+    const data = await readFile(dataSource, { encoding: "utf8" });
+    list = data.split("\n");
+  } catch (err) {}
+
+  res.json({ contatos: list });
+});
+
+router.delete("/contato", async (req, res) => {
+  const name = req.query.name as string;
+
+  if (!name) {
+    res.json({ error: "Nome invalido." });
+    return;
+  }
+
+  let list: string[] = [];
+  try {
+    const data = await readFile(dataSource, { encoding: "utf8" });
+    list = data.split("\n");
+  } catch (err) {}
+
+  list = list.filter(
+    (item) => item.toLowerCase() !== (name as string).toLowerCase()
+  );
+
+  await writeFile(dataSource, list.join("\n"));
+
+  res.json({ contato: name });
 });
 
 export default router;
