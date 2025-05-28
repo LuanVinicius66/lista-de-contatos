bf5b151fda6ad2c859ff167c565fb32665376eca
Luan <luan.santos@rspneus.com.br>
2025-05-28 16:08:15 -0300

    Validação de autorização

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -4,14 +4,15 @@ import {
   deleteContactController,
   getContactsController,
 } from "../controllers/contactController";
+import { privateRequest } from "../middlewares/auth";
 
 const router = express.Router();
 
 //Listar todos os contatos
 router.get("/contatos", getContactsController);
 //Criar novos contatos
-router.post("/contato", createContactController);
+router.post("/contato", privateRequest, createContactController);
 //Deletar contatos
-router.delete("/contato", deleteContactController);
+router.delete("/contato", privateRequest, deleteContactController);
 
 export default router;
